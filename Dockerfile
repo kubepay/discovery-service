ff11cb8fb6b221093744f3434ea09ea98690c086
Abhishek Pandey <abhiesa@gmail.com>
2018-01-31 22:36:58 +0530

    updating Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ MAINTAINER Abhishek Pandey <abhiesa@outlook.com>
 ADD ./target/discovery-service.jar /app/
 CMD ["java", "-Xmx200m", "-jar", "/app/discovery-service.jar"]
 
+
 HEALTHCHECK --interval=10s --timeout=3s CMD curl -f http://localhost:8888/health || exit 1
 EXPOSE 8761
--- deploy_travisci.sh
+++ deploy_travisci.sh
@@ -8,7 +8,7 @@ sleep 15 # wait for automated Docker Hub build to finish...
 
 IMAGE="abhiesa/discovery-service"
 IMAGE_TAG="2.0.${TRAVIS_BUILD_NUMBER}"
-mvn clean install -DskipTests=true
+
 docker build --file=Dockerfile --tag=${IMAGE}:${IMAGE_TAG} --rm=true .
 docker push ${IMAGE}:${IMAGE_TAG}
 docker tag ${IMAGE}:${IMAGE_TAG} ${IMAGE}:latest
