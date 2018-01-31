2a8d1dc9df107d66de7f4f56bdc5df06911e7f1f
Abhishek Pandey <abhiesa@gmail.com>
2018-01-31 22:36:13 +0530

    updating docker script

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM openjdk:8-jre-alpine
 MAINTAINER Abhishek Pandey <abhiesa@outlook.com>
 
-ADD ./target/config-service.jar /app/
+ADD ./target/discovery-service.jar /app/
 CMD ["java", "-Xmx200m", "-jar", "/app/discovery-service.jar"]
 
 HEALTHCHECK --interval=10s --timeout=3s CMD curl -f http://localhost:8888/health || exit 1
--- deploy_travisci.sh
+++ deploy_travisci.sh
@@ -1,38 +1,14 @@
-#!/bin/sh
-
-# Builds and deploys JAR build artifact to GitHub (acts as binary repository)
-# Builds immutable Docker Image, deploying the JAR, above.
-
-# Uses encrypted environment variables in .travis.yml file used here
-# travis encrypt GH_TOKEN=<your_token_hash> --add
-# travis encrypt COMMIT_AUTHOR_EMAIL=<your_email_here> --add
-# travis encrypt GH_ARTIFACT_REPO=github.com/<your_repo_path>.git --add
-# travis encrypt DOCKER_USERNAME=<your_username> --add
-# travis encrypt DOCKER_PASSWORD=<your_password> --add
-
-#set -x
-
-# Builds and deploys JAR build artifact to GitHub (acts as binary repository)
-# cd build/libs
-# git init
-# git config user.name "travis-ci"
-# git config user.email "abhiesa@gmail.com"
-
-# git add *.jar
-# git commit -m "Deploy Travis CI Build #${TRAVIS_BUILD_NUMBER} artifacts to GitHub"
-# git push --force --quiet "https://${GH_TOKEN}@${GH_ARTIFACT_REPO}" master:build-artifacts > /dev/null 2>&1
-
 # Builds immutable Docker Image, deploying the JAR, above.
 cd -
 docker login -u="abhiesa" -p="${DOCKER_PASSWORD}"
 
 set -ex
 
-sleep 120 # wait for automated Docker Hub build to finish...
+sleep 15 # wait for automated Docker Hub build to finish...
 
 IMAGE="abhiesa/discovery-service"
 IMAGE_TAG="2.0.${TRAVIS_BUILD_NUMBER}"
-
+mvn clean install -DskipTests=true
 docker build --file=Dockerfile --tag=${IMAGE}:${IMAGE_TAG} --rm=true .
 docker push ${IMAGE}:${IMAGE_TAG}
 docker tag ${IMAGE}:${IMAGE_TAG} ${IMAGE}:latest
